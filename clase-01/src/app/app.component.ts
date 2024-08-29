8e19d1d7ec9d51cd5218b67ddfeb2ff1b246ee0f
Angeles-G <b.belengarcia2028@gmail.com>
2024-08-29 18:25:23 -0300

    ejercicio1

---
+++ clase-01/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'clase-01';
+
+  edadUno: number = 0;
+  edadDos: number = 0;
+
+  promedio: number = 0;
+  suma: number = 0;
+
+  calcularPromedio() {
+    return this.promedio = Number(this.calcularSuma()) / 2;
+  }
+  calcularSuma() {
+    return this.suma = Number(this.edadUno) + Number(this.edadDos);
+  }
+  limpiar() {
+    this.suma = 0;
+    this.promedio = 0;
+    this.edadDos = 0;
+    this.edadUno = 0;
+  }
+}
