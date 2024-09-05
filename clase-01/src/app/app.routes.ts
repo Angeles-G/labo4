186ef0ce3497b97ede1fae6e01ecc6f8f39ac8ad
Angeles-G <b.belengarcia2028@gmail.com>
2024-09-05 11:47:13 -0300

    ruteo

--- clase-01/src/app/app.routes.ts
+++ clase-01/src/app/app.routes.ts
@@ -3,16 +3,31 @@ import { BienvenidoComponent } from './bienvenido/bienvenido.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { NgModel } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { UsuarioComponent } from './usuario/usuario.component';
 
 export const routes: Routes = [
-    { path: 'Bienvenido', component: BienvenidoComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'usuario', component: UsuarioComponent },
-    { path: 'error', component: ErrorComponent },
-    { path: '', redirectTo: 'Bienvenido', pathMatch: "full"}, // Ruta por defecto
-    { path: '**', redirectTo: 'error', pathMatch:"full"} // Ruta para no encontrados
+    { path: "Bienvenido", loadComponent: () => import('./bienvenido/bienvenido.component').then((m) => m.BienvenidoComponent) },
+    // { path: 'Bienvenido', component: BienvenidoComponent },
+    { path: "login", loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent) },
+    // { path: 'login', component: LoginComponent },
+    { path: "usuario", loadComponent: () => import('./usuario/usuario.component').then((m) => m.UsuarioComponent) },
+    // { path: 'usuario', component: UsuarioComponent },
+    { path: "error", loadComponent: () => import('./error/error.component').then((m) => m.ErrorComponent) },
+    // { path: 'error', component: ErrorComponent },
+    {
+        path: "auth", loadComponent: () => import('./auth/auth.component').then((m) => m.AuthComponent),
+        children:
+            [
+                {
+                    path: 'login',
+                    loadComponent: () => import('../login/login.component').then((m) => m.LoginComponent),
+                }
+            ]
+
+    },
+    { path: '', redirectTo: 'Bienvenido', pathMatch: "full" }, // Ruta por defecto
+    { path: '**', redirectTo: 'error', pathMatch: "full" } // Ruta para no encontrados
 ];
 
 @NgModule({
