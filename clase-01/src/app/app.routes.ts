344ceb8194c954c135280c4dd29afc72dea63e91
Angeles-G <b.belengarcia2028@gmail.com>
2024-09-05 09:27:31 -0300

    ejercicio2

--- clase-01/src/app/app.routes.ts
+++ clase-01/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+import { NgModel } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { UsuarioComponent } from './usuario/usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Bienvenido', component: BienvenidoComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'usuario', component: UsuarioComponent },
+    { path: 'error', component: ErrorComponent },
+    { path: '', redirectTo: 'Bienvenido', pathMatch: "full"}, // Ruta por defecto
+    { path: '**', redirectTo: 'error', pathMatch:"full"} // Ruta para no encontrados
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
